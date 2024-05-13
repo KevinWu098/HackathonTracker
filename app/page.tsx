75ac97c9f3f2fb0ef54ccdf8d607b2de7815184e
Kevin Wu <kevinwu098@gmail.com>
2024-05-13 01:26:38 -0700

    fix: correct imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,77 @@
-'use client'
-import {useEffect, useState} from 'react'
-import {AuroraBackground} from '@/components/ui/aurora-background'
-import yaml from 'js-yaml'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import MyCalender from '@/components/calender'
-import {EventYaml, HackathonEvent} from '@/types/Event'
+"use client";
+import { useEffect, useState } from "react";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import yaml from "js-yaml";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import MyCalender from "@/components/calendar";
+import { EventYaml, HackathonEvent } from "@/types/Event";
 
 export default function Home() {
-  // const events = readYaml('events.yaml')
-  const [events, setEvents] = useState<EventYaml[]>([])
-  const [searchTerm, setSearchTerm] = useState('')
+    // const events = readYaml('events.yaml')
+    const [events, setEvents] = useState<EventYaml[]>([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-    // Get events from public/events.yaml
-    fetch('/events.yaml')
-      .then((response) => response.text())
-      .then((text) => {
-        const events = yaml.load(text) as EventYaml[]
+    useEffect(() => {
+        // Get events from public/events.yaml
+        fetch("/events.yaml")
+            .then((response) => response.text())
+            .then((text) => {
+                const events = yaml.load(text) as EventYaml[];
 
-        events.forEach((event: EventYaml) => {
-          const startYear = new Date(event.start).getFullYear()
-          const endYear = new Date(event.end).getFullYear()
-          const currentYear = new Date().getFullYear()
+                events.forEach((event: EventYaml) => {
+                    const startYear = new Date(event.start).getFullYear();
+                    const endYear = new Date(event.end).getFullYear();
+                    const currentYear = new Date().getFullYear();
 
-          // Determine if the event's year needs updating
-          const updatedStartDate =
-            startYear === currentYear - 1
-              ? event.start.replace(
-                  startYear.toString(),
-                  (startYear + 1).toString(),
-                )
-              : event.start
-          const updatedEndDate =
-            endYear === currentYear - 1
-              ? event.end.replace(endYear.toString(), (endYear + 1).toString())
-              : event.end
+                    // Determine if the event's year needs updating
+                    const updatedStartDate =
+                        startYear === currentYear - 1
+                            ? event.start.replace(
+                                  startYear.toString(),
+                                  (startYear + 1).toString()
+                              )
+                            : event.start;
+                    const updatedEndDate =
+                        endYear === currentYear - 1
+                            ? event.end.replace(
+                                  endYear.toString(),
+                                  (endYear + 1).toString()
+                              )
+                            : event.end;
 
-          event.originalStartDate = event.start
-          event.originalEndDate = event.end
-          event.yearUpdated =
-            startYear === currentYear - 1 || endYear === currentYear - 1
-          event.start = updatedStartDate
-          event.end = updatedEndDate
-          event.searchMatch = false
-        })
+                    event.originalStartDate = event.start;
+                    event.originalEndDate = event.end;
+                    event.yearUpdated =
+                        startYear === currentYear - 1 ||
+                        endYear === currentYear - 1;
+                    event.start = updatedStartDate;
+                    event.end = updatedEndDate;
+                    event.searchMatch = false;
+                });
 
-        setEvents(events)
-      })
-  }, [])
+                setEvents(events);
+            });
+    }, []);
 
-  const getHighlightedEvents = (): HackathonEvent[] => {
-    return events.map((event: EventYaml) => ({
-      ...event,
-      title: event.name,
-      start: new Date(event.start),
-      end: new Date(event.end),
-      searchMatch: searchTerm
-        ? event.college.toLowerCase().includes(searchTerm.toLowerCase())
-        : false,
-    }))
-  }
+    const getHighlightedEvents = (): HackathonEvent[] => {
+        return events.map((event: EventYaml) => ({
+            ...event,
+            title: event.name,
+            start: new Date(event.start),
+            end: new Date(event.end),
+            searchMatch: searchTerm
+                ? event.college.toLowerCase().includes(searchTerm.toLowerCase())
+                : false,
+        }));
+    };
 
-  return (
-    <div>
-      <AuroraBackground>
-        <div className="flex flex-col items-center justify-between p-24 bg-black w-screen h-screen">
-          <MyCalender events={getHighlightedEvents()} />
+    return (
+        <div>
+            <AuroraBackground>
+                <div className="flex flex-col items-center justify-between p-24 bg-black w-screen h-screen">
+                    <MyCalender events={getHighlightedEvents()} />
+                </div>
+            </AuroraBackground>
         </div>
-      </AuroraBackground>
-    </div>
-  )
+    );
 }
--- components/calendar.tsx
+++ components/calendar.tsx
@@ -1,116 +1,118 @@
-'use client'
-import {useEffect, useState, FC} from 'react'
-import {AuroraBackground} from '@/components/ui/aurora-background'
-import moment from 'moment-timezone'
-import {Calendar, momentLocalizer} from 'react-big-calendar'
-import {Button} from '@/components/ui/button'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import {Separator} from '@/components/ui/separator'
-import {HackathonEvent} from '@/types/Event'
+"use client";
+import { useEffect, useState, FC } from "react";
+import { AuroraBackground } from "@/components/ui/aurora-background";
+import moment from "moment-timezone";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Button } from "@/components/ui/button";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { Separator } from "@/components/ui/separator";
+import { HackathonEvent } from "@/types/Event";
 
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/components/ui/dialog'
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
-const MyCalender: FC<{events: HackathonEvent[]}> = ({events}) => {
-  const [selectedEvent, setSelectedEvent] = useState<HackathonEvent | null>(
-    null,
-  ) // Selected event
-  const [open, setOpen] = useState(false) // Modal open state
+const MyCalender: FC<{ events: HackathonEvent[] }> = ({ events }) => {
+    const [selectedEvent, setSelectedEvent] = useState<HackathonEvent | null>(
+        null
+    ); // Selected event
+    const [open, setOpen] = useState(false); // Modal open state
 
-  const eventStyleGetter = (
-    event: HackathonEvent,
-    start: any,
-    end: any,
-    isSelected: any,
-  ) => {
-    // Set the default background color based on the event type
-    let backgroundColor = event.type === 'hackathon' ? 'gold' : 'lightblue' // Gold for hackathons, blue for conferences
+    const eventStyleGetter = (
+        event: HackathonEvent,
+        start: any,
+        end: any,
+        isSelected: any
+    ) => {
+        // Set the default background color based on the event type
+        let backgroundColor = event.type === "hackathon" ? "gold" : "lightblue"; // Gold for hackathons, blue for conferences
 
-    if (event.searchMatch) {
-      backgroundColor = 'red' // If the event matches the search term
-    } else if (event.yearUpdated) {
-      backgroundColor = 'grey' // If the year was updated
-    }
+        if (event.searchMatch) {
+            backgroundColor = "red"; // If the event matches the search term
+        } else if (event.yearUpdated) {
+            backgroundColor = "grey"; // If the year was updated
+        }
 
-    let newStyle = {
-      backgroundColor: backgroundColor,
-      color: 'black',
-      borderRadius: '0px',
-      border: 'none',
-    }
+        let newStyle = {
+            backgroundColor: backgroundColor,
+            color: "black",
+            borderRadius: "0px",
+            border: "none",
+        };
 
-    return {
-      style: newStyle,
-    }
-  }
+        return {
+            style: newStyle,
+        };
+    };
 
-  const handleEventClick = (event: HackathonEvent) => {
-    setSelectedEvent(event) // Set the clicked event as the selected event
-    setOpen(true) // Open the modal
-  }
+    const handleEventClick = (event: HackathonEvent) => {
+        setSelectedEvent(event); // Set the clicked event as the selected event
+        setOpen(true); // Open the modal
+    };
 
-  return (
-    <div className="flex flex-col z-50 justify-center gap-10">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center text-white">
-        Calendar
-      </h1>
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{height: 500, backgroundColor: 'white'}}
-        eventPropGetter={eventStyleGetter}
-        onSelectEvent={handleEventClick} // Handle event click
-      />
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>{selectedEvent?.name}</DialogTitle>
-            <DialogDescription>
-              <div className="flex flex-row">
-                <div className="flex flex-col w-full">
-                  College: {selectedEvent?.college}
-                  <Separator />
-                  Dates:{' '}
-                  {selectedEvent?.yearUpdated
-                    ? ` Original: ${moment(
-                        selectedEvent?.originalStartDate,
-                      ).format('MMM Do YYYY')} - `
-                    : `${moment(selectedEvent?.start).format(
-                        'MMM Do YYYY',
-                      )} - `}
-                  {selectedEvent?.yearUpdated
-                    ? `${moment(selectedEvent?.originalEndDate).format(
-                        'MMM Do YYYY',
-                      )}`
-                    : moment(selectedEvent?.end).format('MMM Do YYYY')}{' '}
-                </div>
-                <div className="flex flex-col align-center justify-center h-full w-full mx-auto">
-                  <Button
-                    onClick={() => {
-                      window.open(selectedEvent?.url)
-                    }}
-                    className="mx-auto"
-                  >
-                    Link
-                  </Button>
-                </div>
-              </div>
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
-    </div>
-  )
-}
+    return (
+        <div className="flex flex-col z-50 justify-center gap-10">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center text-white">
+                Calendar
+            </h1>
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500, backgroundColor: "white" }}
+                eventPropGetter={eventStyleGetter}
+                onSelectEvent={handleEventClick} // Handle event click
+            />
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>{selectedEvent?.name}</DialogTitle>
+                        <DialogDescription>
+                            <div className="flex flex-row">
+                                <div className="flex flex-col w-full">
+                                    College: {selectedEvent?.college}
+                                    <Separator />
+                                    Dates:{" "}
+                                    {selectedEvent?.yearUpdated
+                                        ? ` Original: ${moment(
+                                              selectedEvent?.originalStartDate
+                                          ).format("MMM Do YYYY")} - `
+                                        : `${moment(
+                                              selectedEvent?.start
+                                          ).format("MMM Do YYYY")} - `}
+                                    {selectedEvent?.yearUpdated
+                                        ? `${moment(
+                                              selectedEvent?.originalEndDate
+                                          ).format("MMM Do YYYY")}`
+                                        : moment(selectedEvent?.end).format(
+                                              "MMM Do YYYY"
+                                          )}{" "}
+                                </div>
+                                <div className="flex flex-col align-center justify-center h-full w-full mx-auto">
+                                    <Button
+                                        onClick={() => {
+                                            window.open(selectedEvent?.url);
+                                        }}
+                                        className="mx-auto"
+                                    >
+                                        Link
+                                    </Button>
+                                </div>
+                            </div>
+                        </DialogDescription>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
+};
 
-export default MyCalender
+export default MyCalender;
